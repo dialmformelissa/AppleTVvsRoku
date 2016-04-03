fe95fcd508a6e2ae69fcd5380e7095ecdca6edbf
Melissa Hanson <dialmformelissa@gmail.com>
2016-04-03 16:45:53 -0400

    second try

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,15 @@
 $(document).ready(function(){
 
-var cost = new Array();
-cost["Yes"]=10;
-cost["No"]=0;
+	$(".feature").on("click", function(e) {
 
-function getcost() {
-	var costvalue=0;
-	var theform = document.forms["featureform"];
-	var selectedCost = theform.elements["selectedcost"];
-	for(var i=0; i<selectedcost.length; i++) {
-		if(selectedCost[i].checked) {
-			costvalue = cost[selectedcost[i].value];
-			break;
-		}
-	}
-return costvalue;
-}
-function showTotal()
-{
-	var divobj = document.getElementById('roku-percent');
-    divobj.style.display='block';
-    divobj.innerHTML = costvalue;
-}
+		var target = $(e.target);
+		var src = target.attr("src");
 
-// function calcscore(){
-//     var num = 0;
-//     $(".feature:checked").each(function(){
-//         num+=parseInt($(this).val(),10);
-//     });
-//     $("input[name=sum]").val(num)
-// }
-// $().ready(function(){
-//     $(".feature").change(function(){
-//         calcscore()
-//     });
-// });
+		$(".lightbox").attr("src");
+
+		$(".lightbox img").attr("src", src);
+
+
+	});
 
 });
